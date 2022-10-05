0fe0e27e7c4395358c1222dfcc0201481fa76376
Davide Passerò <shardonian86@gmail.com>
2022-10-05 14:40:12 +0200

    routing and styled components globalStyles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route, } from "react-router-dom";
+
+import Home from './pages/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyles } from "../components/styles/GlobalStyles";
+
+function Home() {
+    return(
+        <>
+        <GlobalStyles />
+        <h1>Hello World!</h1>
+        </>
+    )
+}
+
+export default Home;
